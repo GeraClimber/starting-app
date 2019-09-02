dcd94dd6532261765cdd0cbb6a19953be6316cb0
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-09-02 23:24:18 +0300

    add routs and components dashboard and hero detail

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,9 +16,12 @@ export class HeroService {
   // TODO: send the message _after_ fetching the heroes
   this.messageService.add('HeroService: fetched heroes');
   return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
   
 
-
-
-}
